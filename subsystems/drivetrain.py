8f822de20f290ba4da56500631cd9892f668a807
BerkeSinanYetkin <71657012+BerkeSinanYetkin@users.noreply.github.com>
2022-08-09 22:04:56 +0300

    azıcık değiştirilmiş drivetrain kodu, ramsete için

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -8,9 +8,9 @@ class DriveTrain:
     drive_fRight: wpilib.PWMVictorSPX
     drive_rRight: wpilib.PWMVictorSPX
 
-    throttle_y = magicbot.will_reset_to(0) # robotun Y ekseninde ilerlemesi
-    throttle_x = magicbot.will_reset_to(0) # robotun X ekseninde ilerlemesi
-    rotation = magicbot.will_reset_to(0)
+    throttle_y = 0 # robotun Y ekseninde ilerlemesi
+    throttle_x = 0 # robotun X ekseninde ilerlemesi
+    rotation = 0
 
     throttle_left_volts = magicbot.will_reset_to(0)
     throttle_right_volts = magicbot.will_reset_to(0)
@@ -28,29 +28,26 @@ class DriveTrain:
             self.drive_rRight,
         )
 
-    def move(self, throttle_y, throttle_x, rotation):
+    def move(self, throttle_y: float, throttle_x: float, rotation: float):
         self.throttle_y = throttle_y
         self.throttle_x = throttle_x
         self.rotation = rotation
-    
-    def stop(self):
-        self.throttle_y = 0
-        self.throttle_x = 0
-        self.rotation = 0
 
     def tank_move(self, throttle_left_volts, throttle_right_volts):
         self.tank_drive = True
 
         self.throttle_left_volts = throttle_left_volts
         self.throttle_right_volts = throttle_right_volts
-    
+
+        self.drive_fLeft.setVoltage(self.throttle_left_volts)
+        self.drive_rLeft.setVoltage(self.throttle_left_volts)
+        self.drive_fRight.setVoltage(self.throttle_right_volts)
+        self.drive_rRight.setVoltage(self.throttle_right_volts)
+        
     def execute(self):
         if not self.tank_drive:
             self.drive.driveCartesian(-self.throttle_y, self.throttle_x, self.rotation, 0)
         
         else:
-            self.drive_fLeft.setVoltage(self.throttle_left_volts)
-            self.drive_rLeft.setVoltage(self.throttle_left_volts)
-            self.drive_fRight.setVoltage(self.throttle_right_volts)
-            self.drive_rRight.setVoltage(self.throttle_right_volts)
+            pass
 
